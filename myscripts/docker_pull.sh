0fec8b24a6182be177c3d0664441456c1e86db3f
mvkreddi <87845009+mvkreddi@users.noreply.github.com>
2022-02-10 17:57:34 +0530

    Update docker_pull.sh

--- myscripts/docker_pull.sh
+++ myscripts/docker_pull.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Pulling from Amazon ECR..."
-docker pull 116117548704.dkr.ecr.us-east-2.amazonaws.com/aws-project:latest
+docker pull 116117548704.dkr.ecr.us-west-2.amazonaws.com/docker-repo:latest
